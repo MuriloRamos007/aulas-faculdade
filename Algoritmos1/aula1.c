b2a48cbd1734988081fd3e27e61b08f30eb01012
MuriloRamos007 <murilowiirr@gmail.com>
2026-03-16 20:49:47 -0400

    compilação e mudança no primeiro arquivo, criando um multiplicador de 2 numeros inteiros

--- Algoritmos1/aula1.c
+++ Algoritmos1/aula1.c
@@ -1,7 +1,24 @@
-
+#include<stdio.h>
 
 int main(){
 	
+	int a, b, multi;
 	
+	printf("Multiplex - seu multiplicador de números inteiros pessoal\n\n");
+	
+	printf("Digite o primeiro número inteiro: ");
+	scanf("%d", &a);
+	
+	printf("Digite o segundo número inteiro: ");
+	scanf("%d", &b);
+	
+	multi = a*b;
+	
+	printf("\nResultado de %d X %d: %d\n------------------------\n", a, b, multi);
+	
+	getchar();
+	getchar();
+	
+	return 0;
 	
 }
