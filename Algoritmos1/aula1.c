cf503f11a273adea7dfbd6fea5c72d4cc5bfad35
MuriloRamos007 <murilowiirr@gmail.com>
2026-03-16 20:59:56 -0400

    mudança no visual do texto e design

--- Algoritmos1/aula1.c
+++ Algoritmos1/aula1.c
@@ -1,24 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-	
-	int a, b, multi;
-	
-	printf("Multiplex - seu multiplicador de números inteiros pessoal\n\n");
-	
-	printf("Digite o primeiro número inteiro: ");
-	scanf("%d", &a);
-	
-	printf("Digite o segundo número inteiro: ");
-	scanf("%d", &b);
-	
-	multi = a*b;
-	
-	printf("\nResultado de %d X %d: %d\n------------------------\n", a, b, multi);
-	
-	getchar();
-	getchar();
-	
-	return 0;
-	
+
+    int a, b, multi;
+
+    system("color 0A");
+    system("cls");
+
+    printf("=========================================\n");
+    printf("             MULTIPLEX\n");
+    printf("=========================================\n\n");
+
+    printf("[1] Digite o primeiro numero: ");
+    scanf("%d", &a);
+
+    printf("[2] Digite o segundo numero: ");
+    scanf("%d", &b);
+
+    multi = a * b;
+
+    printf("\n=========================================\n");
+    printf(" Resultado Final\n");
+    printf(" %d x %d = %d\n", a, b, multi);
+    printf("=========================================\n");
+
+    printf("\nPressione ENTER para finalizar...");
+    getchar();
+    getchar();
+
+    return 0;
 }
